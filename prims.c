2366b7ff05b242f2d508bbf5a62f6a656829611c
kris321k <rajendranaik6622@gmail.com>
2024-03-23 13:45:25 +0530

    new_madf

---
+++ prims.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#define cost 5
+
+int travelling_sales(int *vertices)
+{
+    if(vertices[])
+}
+
+int main()
+{
+    printf("enter the matrix\n");
+}
